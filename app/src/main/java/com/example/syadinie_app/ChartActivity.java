e00207c57bb87d354b4b8d78fd1d4fc0dd240cfe
dina <fardinafauzi@gmail.com>
2026-07-17 12:38:34 +0800

    edit intrface

--- app/src/main/java/com/example/syadinie_app/ChartActivity.java
+++ app/src/main/java/com/example/syadinie_app/ChartActivity.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
@@ -42,21 +41,24 @@ public class ChartActivity extends BaseActivity {
                 if (tab.getPosition() == 0) {
                     pieChartState.setVisibility(View.VISIBLE);
                     pieChartGender.setVisibility(View.GONE);
-                } else if (tab.getPosition() == 1) {
+                } else {
                     pieChartState.setVisibility(View.GONE);
                     pieChartGender.setVisibility(View.VISIBLE);
                 }
             }
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {}
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
 
             @Override
-            public void onTabReselected(TabLayout.Tab tab) {}
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
         });
     }
 
     private void loadStateChart() {
+
         ArrayList<PieEntry> entries = new ArrayList<>();
         Cursor cursor = db.getStateStatistics();
 
@@ -68,22 +70,35 @@ public class ChartActivity extends BaseActivity {
                 entries.add(new PieEntry(total, state));
             }
         }
+
         cursor.close();
 
         if (entries.isEmpty()) return;
 
         PieDataSet dataSet = new PieDataSet(entries, "");
-        dataSet.setColors(
-                Color.RED,
-                Color.BLUE,
-                Color.GREEN,
-                Color.YELLOW,
-                Color.MAGENTA,
-                Color.CYAN,
-                Color.GRAY,
-                Color.DKGRAY
-        );
-        dataSet.setSliceSpace(2f);
+
+        ArrayList<Integer> colors = new ArrayList<>();
+
+        colors.add(Color.parseColor("#5DADE2")); // Sky Blue
+        colors.add(Color.parseColor("#58D68D")); // Emerald Green
+        colors.add(Color.parseColor("#AF7AC5")); // Purple
+        colors.add(Color.parseColor("#F5B041")); // Gold
+        colors.add(Color.parseColor("#EC7063")); // Coral Red
+        colors.add(Color.parseColor("#48C9B0")); // Turquoise
+        colors.add(Color.parseColor("#5499C7")); // Ocean Blue
+        colors.add(Color.parseColor("#F1948A")); // Soft Pink
+        colors.add(Color.parseColor("#BB8FCE")); // Lavender
+        colors.add(Color.parseColor("#52BE80")); // Green
+        colors.add(Color.parseColor("#F7DC6F")); // Yellow
+        colors.add(Color.parseColor("#45B39D")); // Teal
+        colors.add(Color.parseColor("#DC7633")); // Orange
+        colors.add(Color.parseColor("#7FB3D5")); // Light Blue
+
+        dataSet.setColors(colors);
+
+        dataSet.setColors(colors);
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(8f);
 
         PieData data = new PieData(dataSet);
         data.setValueTextSize(14f);
@@ -91,17 +106,36 @@ public class ChartActivity extends BaseActivity {
 
         pieChartState.setData(data);
         pieChartState.getDescription().setEnabled(false);
-        pieChartState.getLegend().setTextColor(Color.WHITE);
+
+        pieChartState.setUsePercentValues(false);
+        pieChartState.setDrawEntryLabels(true);
         pieChartState.setEntryLabelColor(Color.WHITE);
+        pieChartState.setEntryLabelTextSize(12f);
+
+        pieChartState.setCenterText("States");
+        pieChartState.setCenterTextColor(Color.WHITE);
+        pieChartState.setCenterTextSize(18f);
+
+        pieChartState.setHoleRadius(50f);
+        pieChartState.setTransparentCircleRadius(55f);
         pieChartState.setHoleColor(Color.TRANSPARENT);
+
+        pieChartState.getLegend().setTextColor(Color.WHITE);
+        pieChartState.getLegend().setTextSize(13f);
+
+        pieChartState.animateY(1200);
         pieChartState.invalidate();
     }
+
     private void loadGenderChart() {
+
         ArrayList<PieEntry> entries = new ArrayList<>();
         Cursor cursor = db.getGenderStatistics();
 
         if (cursor != null) {
+
             while (cursor.moveToNext()) {
+
                 String gender = cursor.getString(0);
                 int total = cursor.getInt(1);
 
@@ -109,6 +143,7 @@ public class ChartActivity extends BaseActivity {
                     entries.add(new PieEntry(total, gender));
                 }
             }
+
             cursor.close();
         }
 
@@ -117,10 +152,12 @@ public class ChartActivity extends BaseActivity {
         PieDataSet dataSet = new PieDataSet(entries, "");
 
         ArrayList<Integer> colors = new ArrayList<>();
-        colors.add(Color.parseColor("#42A5F5")); // Biru untuk Male
-        colors.add(Color.parseColor("#EC407A")); // Pink untuk Female
+        colors.add(Color.parseColor("#6EC6FF")); // Male
+        colors.add(Color.parseColor("#FF9EC7")); // Female
+
         dataSet.setColors(colors);
-        dataSet.setSliceSpace(2f);
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(8f);
 
         PieData data = new PieData(dataSet);
         data.setValueTextSize(14f);
@@ -128,9 +165,22 @@ public class ChartActivity extends BaseActivity {
 
         pieChartGender.setData(data);
         pieChartGender.getDescription().setEnabled(false);
-        pieChartGender.getLegend().setTextColor(Color.WHITE);
-        pieChartGender.setEntryLabelColor(Color.WHITE);
+
+        pieChartGender.setCenterText("Gender");
+        pieChartGender.setCenterTextColor(Color.WHITE);
+        pieChartGender.setCenterTextSize(18f);
+
+        pieChartGender.setHoleRadius(50f);
+        pieChartGender.setTransparentCircleRadius(55f);
         pieChartGender.setHoleColor(Color.TRANSPARENT);
+
+        pieChartGender.setEntryLabelColor(Color.WHITE);
+        pieChartGender.setEntryLabelTextSize(12f);
+
+        pieChartGender.getLegend().setTextColor(Color.WHITE);
+        pieChartGender.getLegend().setTextSize(13f);
+
+        pieChartGender.animateY(1200);
         pieChartGender.invalidate();
     }
 }
